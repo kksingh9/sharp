69c80d0bb6d0aafab36d547d272cfb3d4fe39bdc
kksingh9 <kksingh0921@gmail.com>
2023-04-24 19:04:29 +0530

    delete and filter

--- delete fun/delet.js
+++ delete fun/delet.js
@@ -1,10 +1,12 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
-
+var filter = document.getElementById('filter');
 //form submit event
 form.addEventListener('submit',additems);
 
 itemList.addEventListener('click',removeItem);
+// filter event
+filter.addEventListener('keyup',filterItems);
 
 //Add items
 function additems(e){
@@ -12,6 +14,7 @@ function additems(e){
 
     //get input value
     var newItem = document.getElementById('item').value;
+    var neItem = document.getElementById('item1').value;
 
     //create new li element
     var li = document.createElement('li');
@@ -19,6 +22,7 @@ function additems(e){
     li.className = "list-group-item";
     //Add text node with input value
     li.appendChild(document.createTextNode(newItem));
+    li.appendChild(document.createTextNode(neItem));
 
     //create del button element
     var deleteBtn = document.createElement('button');
@@ -30,15 +34,15 @@ function additems(e){
     li.appendChild(deleteBtn);
     
     // Add edit button
-    var editBtn = document.createElement('button');
+    var editBtn1 = document.createElement('button');
 
     //Add classes to edit button
-    editBtn.className = "btn btn-danger btn-sm float-right delete";
-    editBtn.appendChild(document.createTextNode('edit'));
+    editBtn1.className = "btn btn-sm float-right editBtn";
+    editBtn1.appendChild(document.createTextNode('edit'));
 
 
     // append edit button to li
-    li.appendChild(editBtn);
+    li.appendChild(editBtn1);
     //append li to item list
     itemList.appendChild(li);
 }
@@ -52,4 +56,27 @@ function removeItem(e){
             itemList.removeChild(li);
         }
     }
+}
+
+// filter items
+function filterItems(e){
+    //convert text to Lowercase
+   var text =  e.target.value.toLowerCase();
+   //var text1 =  e1.target.value.toLowerCase();
+   //console.log(text);
+   // get lis
+    var items = itemList.getElementsByTagName('li');
+    // convert to an array
+   // console.log(items);
+    Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        var item1 = item.childNodes[1].textContent;
+        
+       // console.log(itemName);
+       if(itemName.toLowerCase().indexOf(text) != -1 || item1.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+       }else{
+        item.style.display = 'none';
+       }
+    });
 }
